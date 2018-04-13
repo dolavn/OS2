839ba8c3ab47023d0251e3696bfd6e533864453e
nitay dolav <dolavn@cs302six1-6.cs.bgu.ac.il>
2018-04-13 15:26:21 +0300

    Added handleStop and handleCont

--- signal.c
+++ signal.c
@@ -1,2 +1,26 @@
 #include "signal.h"
+#include "proc.h"
 
+int handleStop(int pid){
+  struct proc* currProc = myproc();
+  if(currProc->pid!=pid){
+    panic("sig stop");
+  }
+  if(currProc->state==RUNNING || currProc->state==RUNNABLE){
+    currProc->state=FROZEN;
+    return 0;
+  }
+  return -1;
+}
+
+int handleCont(int pid){
+  struct proc* currProc = myproc();
+  if(currProc->pid!=pid){
+    panic("sig stop");
+  }
+  if(currProc->state==FROZEN){
+    currProc->state=RUNNABLE;
+    return 0;
+  }
+  return -1;
+}
